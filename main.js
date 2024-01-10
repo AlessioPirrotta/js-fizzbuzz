c990d6b7fbb4d5833842271ae40ffd655fec3e96
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-10 09:03:33 +0100

    bonus completed

--- main.js
+++ main.js
@@ -2,28 +2,41 @@
 
 let container = document.getElementById("cntr")
 
+
  for(let i=1; i<=100; i++ )
  {
 
     if(i % 3 === 0 && i % 5 === 0 ) {
         console.log("fizzbuzz")
-        container.append("fizzbuzz")
+        let nuovoElemento = document.createElement("div")
+        nuovoElemento.textContent = "fizzbuzz"
+        nuovoElemento.className=("bg-warning px-4 py-3 my-5 mx-3")
+        container.append(nuovoElemento)
     }
 
     else if(i % 3 === 0) {
         console.log("fizz")
-        container.append("fizz")
+        let nuovoElemento = document.createElement("div")
+        nuovoElemento.textContent = "fizz"
+        nuovoElemento.className=("bg-primary px-4 py-3 my-5 mx-3")
+        container.append(nuovoElemento)
     }
 
     else if(i % 5 === 0) {
         console.log("buzz")
-        container.append("buzz")
+        let nuovoElemento = document.createElement("div")
+        nuovoElemento.textContent = "buzz"
+        nuovoElemento.className=("text-success bg-dark px-4 py-3 my-5 mx-3")
+        container.append(nuovoElemento)
     }
 
     else{
         console.log(i)
-        let element = (i)
-        container.append(element)
+        let nuovoElemento = document.createElement("div")
+        nuovoElemento.textContent = (i)
+        nuovoElemento.className=("text-danger bg-secondary px-4 py-3 my-5 mx-3")
+        container.append(nuovoElemento)
+
     }
     
     
